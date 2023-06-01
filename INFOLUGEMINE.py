cbfed5af85befdc80cf94cef3b9cf2816b3070ee
andreeammer <125971323+andreeammer@users.noreply.github.com>
2023-06-01 14:27:14 +0300

    Update INFOLUGEMINE.py

--- INFOLUGEMINE.py
+++ INFOLUGEMINE.py
@@ -1,4 +1,4 @@
-import random
+from datetime import datetime, timedelta
 
 
 def is_fastest_lap(driver_name, fastest_data):
@@ -8,19 +8,12 @@ def is_fastest_lap(driver_name, fastest_data):
         return ""
 
 
-def sec2time(sec, n_msec=3):
+def sec2time(sec):
     if hasattr(sec, '__len__'):
         return [sec2time(s) for s in sec]
-    m, s = divmod(sec, 60)
-    h, m = divmod(m, 60)
-    d, h = divmod(h, 24)
-    if n_msec > 0:
-        pattern = '%%02d:%%02d:%%0%d.%df' % (n_msec + 3, n_msec)
-    else:
-        pattern = r'%02d:%02d:%02d'
-    if d == 0:
-        return pattern % (h, m, s)
-    return ('%d days, ' + pattern) % (d, h, m, s)
+    d = datetime(1, 1, 1) + timedelta(seconds=sec)
+    time_format = "%H:%M:%S.%f" if d.microsecond else "%H:%M:%S"
+    return d.strftime(time_format)
 
 
 if __name__ == '__main__':
